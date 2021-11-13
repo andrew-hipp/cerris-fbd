31f1ceeb664d0b1e9768bfc3a947fb88c87d2917
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2021-11-13 07:13:26 -0600

    trying to get densitree type plot to look nice

--- SCRIPTS/00.readData.R
+++ SCRIPTS/00.readData.R
@@ -1,8 +1,9 @@
 ## read in Cerris data
 library(ape)
+library(phytools)
 # library(MCMCtreeR)
 # library(ips)
-dat.sum <- read.table('../DATA/runs1.2.3.5.7.8.9.10.logSummary.txt',
+if(!exists('dat.sum')) dat.sum <- read.table('../DATA/runs1.2.3.5.7.8.9.10.logSummary.txt',
                       header = T, as.is = T, row.names = 1)
 if(!exists('tr.full')) {
   tr.full <- list(
@@ -21,8 +22,17 @@ cleanTree <- function(x, keepGrp = '_0.0', strip = '_0.0', ladder = T) {
   return(x)
 }
 
+set.seed(1)
 tr.clean <- lapply(tr, cleanTree)
 tr.full <- lapply(tr.full, function(x) lapply(x, cleanTree))
+tree.subsample <- lapply(tr.full, function(x) sample(x, size = 200))
 }
 
-plot(tr.clean$r1)
+plot.new()
+lapply(tree.subsample$r1, plotTree,
+      add = T,
+      color = make.transparent('blue', alpha = 0.1),
+      lwd = 0.1,
+      # xlim = c(0, 80),
+      )
+plotTree(tr.clean$r1, add = TRUE)
