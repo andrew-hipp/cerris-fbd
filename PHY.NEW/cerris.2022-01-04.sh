ee581604ce2d3265f230f4c5578af110aa8be288
andrew-hipp <andrew.hipp@gmail.com>
2022-01-04 12:14:13 -0600

    Adding phylo files generated in R

---
+++ PHY.NEW/cerris.2022-01-04.sh
@@ -0,0 +1,3 @@
+raxmlHPC-PTHREADS-SSE3 -f a -T 7 -m GTRGAMMA -x 12345 -p 12345 -# 200 -s cerris.2022-01-04.m15.phy -n cerris.2022-01-04.m15.rax
+raxmlHPC-PTHREADS-SSE3 -f a -T 7 -m GTRGAMMA -x 12345 -p 12345 -# 200 -s cerris.2022-01-04.m20.phy -n cerris.2022-01-04.m20.rax
+raxmlHPC-PTHREADS-SSE3 -f a -T 7 -m GTRGAMMA -x 12345 -p 12345 -# 200 -s cerris.2022-01-04.m25.phy -n cerris.2022-01-04.m25.rax
