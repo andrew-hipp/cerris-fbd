31dc364f887b4fbc6f470e20f61be075cb484172
Andrew Hipp <andrew.hipp@gmail.com>
2022-01-05 10:04:23 -0600

    adding new dates table

--- SCRIPTS/B.01.makeBeastNexus.R
+++ SCRIPTS/B.01.makeBeastNexus.R
@@ -4,19 +4,21 @@ library(ape)
 library(RADami)
 
 read.meta <- TRUE
-
-if(read.meta) {
-  message('... reading metadata ...')
-  dat.meta <- read.xlsx('../DATA/oakAccessions_cerrisRevision_2020-11-23.xlsx', 1)
-}
-
-fossilAge <- 'min' # exported once as min, once as max...
-  ### ... still to do: runif (2018-10-20 note)
 today <- format(Sys.time(), "%Y-%m-%d")
 minInds <- 10 # minimum number of inds / locus
 singleSpCol <- 'cerrisSingleSpV2' # old one was 'cerrisSingleSp'
-# source('https://raw.githubusercontent.com/andrew-hipp/RADami/master/R/rad2nex.R')
-dat.dating <- read.xlsx('../DATA/cerrisDating2.xlsx', 1)
+
+if(read.meta) {
+  message('... reading metadata ...')
+  dat.meta <- read.xlsx('../DATA/oakAccessions_cerrisRevision_2021-08-06.xlsx', 1)
+  dat.meta$LABEL_taxonOnly <- as.character(dat.meta$LABEL_taxonOnly)
+	dat.meta$"Cleaned_NAMES-USE-THIS" <- as.character(dat.meta$"Cleaned_NAMES-USE-THIS")
+	dat.meta$tipName <- ifelse(is.na(dat.meta$"Cleaned_NAMES-USE-THIS"),
+		                   dat.meta$LABEL_taxonOnly,
+		                   dat.meta$"Cleaned_NAMES-USE-THIS")
+}
+
+dat.dating <- read.xlsx('../DATA/Cerris_dating3_Dec2021.xlsx', sheet = 'beast.table')
 dat.meta[[singleSpCol]] <- as.logical(dat.meta[[singleSpCol]])
 dat.meta[[singleSpCol]][is.na(dat.meta[[singleSpCol]])] <- FALSE
 message('... writing RADseq nexus matrix ...')
@@ -24,10 +26,11 @@ message('... writing RADseq nexus matrix ...')
  radLoc.cerrisTots <- apply(rads$radSummary$inds.mat[radInds.cerrisDating, ], 2, sum) %>% sort
  radLoc.cerris <- which(radLoc.cerrisTots >= minInds) %>% names
  radInds.cerrisNames <-
-  paste(
-    gsub(" | ssp. ", "_", make.unique(dat.meta$tipName[match(radInds.cerrisDating, dat.meta$fastQ.file.name)], sep = '_')),
-    '0.0', sep = '_'
-  )
+    gsub(" | ssp. ", "_",
+      make.unique(
+        dat.meta$tipName[match(radInds.cerrisDating, dat.meta$fastQ.file.name)], sep = '_'
+      ) # close make.unique
+    ) # close gsub
  if(fossilAge == 'max') fossilAge.vector <- dat.dating$ageMax
  if(fossilAge == 'min') fossilAge.vector <- dat.dating$ageMin
  radInds.cerrisFossils <-
