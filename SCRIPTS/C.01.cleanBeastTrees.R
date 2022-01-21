f5d4177ad3994c99a80588c8052d97f749ec374a
andrew-hipp <andrew.hipp@gmail.com>
2022-01-21 15:49:36 -0600

    Cleaned beast trees

---
+++ SCRIPTS/C.01.cleanBeastTrees.R
@@ -0,0 +1,30 @@
+## clean up the beast trees
+
+library(ape)
+datePrecision <- 1
+dopdf = T
+
+trFile <- c(
+  r1 = '../BEAST.FILES/comboRuns.noBurn.annotated_r1.tre',
+  r2 = '../BEAST.FILES/comboRuns.noBurn.annotated_r2.tre'
+)
+
+tr.beast <- lapply(trFile, read.nexus)
+
+tr.beast.extant <- structure(vector('list', 2), .Names = names(tr.beast))
+
+for(i in names(tr.beast)) {
+  taxa.fossils <-
+    which(round(node.depth.edgelength(tr.beast[[i]])[1:Ntip(tr.beast[[i]])], 1) <
+          round(max(node.depth.edgelength(tr.beast[[i]])),1)
+          )
+  tr.beast.extant[[i]] <- drop.tip(tr.beast[[i]], taxa.fossils)
+  if(dopdf) pdf(paste('../OUT/tr.beast', i, 'pdf', sep = '.'), 11.5, 8)
+    layout(matrix(1:2, 1))
+    tr.beast[[i]] %>% ladderize(right = FALSE) %>% plot(cex = 0.5)
+    tr.beast.extant[[i]] %>% ladderize(right = FALSE) %>% plot(cex = 0.7)
+  if(dopdf) dev.off()
+  write.tree(tr.beast.extant[[i]],
+              paste('../OUT/tr.beast.pruned', i, 'tre', sep = '.')
+            )
+}
