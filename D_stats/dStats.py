896fc3049e68de85c8f4e9e559e672a544f6b321
andrew-hipp <andrew.hipp@gmail.com>
2022-05-06 08:32:26 -0500

    Updating test to focus on Q. ilex - Q. suber introgression

--- D_stats/dStats.py
+++ D_stats/dStats.py
@@ -12,3 +12,17 @@ ipyclient = ipp.Client(cluster_id="baba")
 
 locifile = '../DATA/oaksall_v1_2.m15.loci.gz'
 newick = '../PHY.NEW/RAxML_bestTree.cerris.2022-01-04.m15.rax'
+
+bb = ipa.baba(data = locifile, newick = newick)
+
+# Q. suber - Q. ilex test constraints:
+#  p4 - sect. Cyclobalanopsis og
+#  p3 - Q. ilex potential introgressor 1
+#  p2 - Q. suber potential introgressor 2
+
+bb.generate_tests_from_tree(
+    constraint_dict={
+        "p4": ["DM5", "DM20", "DM18", "DM56"],
+        "p3": ["33413_thamno"],
+        "p2": ["OAK-MOR-1144", "OAK-MOR-588.fq.barcodeStripped", "OAK-MOR-985"]
+    })
--- D_stats/getTips.R
+++ D_stats/getTips.R
@@ -4,12 +4,15 @@ library(ape)
 library(phytools)
 library(magrittr)
 
-# og taxa, indls selected based on number of loci recovered:
-#  - Q. calliprinos
-#  - Q. ilex
-#  - Q. monimotricha - DM68
-#  - Q. phillyreoides - OAK-MOR-994
-og <- c('OAK-MOR-1145', 'OAK-MOR-1146', 'DM68', 'OAK-MOR-994')
+# og taxa from sect. Cyclobalanopsis, indls selected based on number of loci recovered:
+#  - Q. blakei - DM5
+#  - Q. championii - DM20
+#  - Q. lamellos - DM18
+#  - Q. gilva - DM56
+
+
+taxa.og <- c("DM5", "DM20", "DM18", "DM56")
+taxa.ilex <- c("OAK-MOR-589.fq.barcodeStripped", "OAK-MOR-980", "OAK-MOR-1146")
 
 # read tree
 tr <- read.tree('../PHY.NEW/RAxML_bestTree.cerris.2022-01-04.m15.rax')
@@ -20,5 +23,8 @@ tipsKeep <-
   '[['(1)
 tipsKeep <- c(og, tr$tip.label[tipsKeep])
 
-tr.cerris <- drop.tip(tr, setdiff(tr$tip.label, tipsKeep))
+tr.cerris <-
+  drop.tip(tr, setdiff(tr$tip.label, tipsKeep)) %>%
+  ladderize
+
 write.tree(tr.cerris, '../D_stats/tr.cerris.tre')
