b53583f311c43be702f8f0714795c1fe8cd7c489
andrew-hipp <andrew.hipp@gmail.com>
2022-05-08 21:35:32 -0500

    Reverting doitall.sh to full analysis

--- D_stats/doitall.sh
+++ D_stats/doitall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# R CMD BATCH getTips.R
+R CMD BATCH getTips.R
 
 ipcluster start -n 64 --cluster-id="baba" --daemonize
 
 gunzip -k ../DATA/cerrisdstat.loci.gz
 
-# python dStats_suberIlex.py
+python dStats_suberIlex.py
 python dStats_cerrisCrenata.py
 
 rm ../DATA/cerrisdstat.loci
