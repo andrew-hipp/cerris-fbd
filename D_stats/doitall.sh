fce580d9d45eb4fb3cf8432ecfe662b4d1ac23b8
andrew-hipp <andrew.hipp@gmail.com>
2022-05-10 21:04:23 -0500

    adding libaniCrenata to doitall.sh

--- D_stats/doitall.sh
+++ D_stats/doitall.sh
@@ -6,8 +6,9 @@ ipcluster start -n 64 --cluster-id="baba" --daemonize
 
 gunzip -k ../DATA/cerrisdstat.loci.gz
 
-python dStats_suberIlex.py
-python dStats_cerrisCrenata.py
+# python dStats_suberIlex.py
+# python dStats_cerrisCrenata.py
+python dStats_libaniCrenata.py
 
 rm ../DATA/cerrisdstat.loci
 
