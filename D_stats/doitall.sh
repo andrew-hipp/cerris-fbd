f6675d36200f4154cef5d357a49bf9c5ce8f214a
andrew-hipp <andrew.hipp@gmail.com>
2022-05-11 20:33:39 -0500

    aegilopsAfares dstat

--- D_stats/doitall.sh
+++ D_stats/doitall.sh
@@ -2,14 +2,15 @@
 
 R CMD BATCH getTips.R
 
-ipcluster start -n 32 --cluster-id="baba" --daemonize
+ipcluster start -n 48 --cluster-id="baba" --daemonize
 
 gunzip -k ../DATA/cerrisdstat.loci.gz
 
 # python dStats_suberIlex.py
 # python dStats_cerrisCrenata.py
-python dStats_libaniCrenata.py
-python dStats_macrolepisCrenata.py
+# python dStats_libaniCrenata.py
+# python dStats_macrolepisCrenata.py
+python dStats_aegilopsAfares.py
 
 rm ../DATA/cerrisdstat.loci
 
