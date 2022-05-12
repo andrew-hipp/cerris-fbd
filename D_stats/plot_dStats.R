9af5d32b3f1ebc4394fd62d139a49a483042008e
andrew-hipp <andrew.hipp@gmail.com>
2022-05-11 21:17:34 -0500

    add Z plot

--- D_stats/plot_dStats.R
+++ D_stats/plot_dStats.R
@@ -8,3 +8,8 @@ dStats.d <- lapply(dStats, '[[', 'dstat')
 pdf('../OUT/dStats.pdf', 11.5, 8)
 boxplot(dStats.d)
 dev.off()
+
+dStats.Z <- lapply(dStats, '[[', 'Z')
+pdf('../OUT/dStats-Z.pdf', 11.5, 8)
+boxplot(dStats.Z)
+dev.off()
