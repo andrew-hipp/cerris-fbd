952666ef7372fdc6bcea434fe448828a97fa921d
andrew-hipp <andrew.hipp@gmail.com>
2022-05-11 21:25:30 -0500

    ordering dStats.d and Z; updating plot

--- D_stats/plot_dStats.R
+++ D_stats/plot_dStats.R
@@ -5,11 +5,19 @@ names(dStats) <- dir(patt = 'bb.dstat.sorted')
 names(dStats) <- gsub('bb.dstat.sorted_|_full.csv', '', names(dStats))
 
 dStats.d <- lapply(dStats, '[[', 'dstat')
+dStats.d <- dStats.d[sapply(dStats.d, mean) %>% order]
+
 pdf('../OUT/dStats.pdf', 11.5, 8)
 boxplot(dStats.d)
 dev.off()
 
-dStats.Z <- lapply(dStats, '[[', 'Z')
+dStats.Z <- lapply(dStats, '[[', 'Z')[names(dStats.d)]
 pdf('../OUT/dStats-Z.pdf', 11.5, 8)
 boxplot(dStats.Z)
+dev.off()
+
+pdf('../OUT/dStats-2panel.pdf', 8, 11.5)
+layout(matrix(1:2, 2))
+boxplot(dStats.d, main = 'D-stat', cex.axis = 0.5)
+boxplot(dStats.Z, main = 'Z', cex.axis = 0.5)
 dev.off()
