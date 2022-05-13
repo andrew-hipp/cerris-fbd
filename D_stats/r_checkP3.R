e97926695c3760039ad08da9f497da57191c5f01
andrew-hipp <andrew.hipp@gmail.com>
2022-05-13 14:55:28 -0500

    fixing Z vs p3

--- D_stats/r_checkP3.R
+++ D_stats/r_checkP3.R
@@ -9,13 +9,13 @@ dat.stat <- read.csv('bb.dstat.sorted_aegilopsAfares_full.csv')
 
 out <- list(
   D = lapply(inds, function(x) {dat.stat$dstat[grep(x, dat.taxa$p3)]}),
-  Z = lapply(inds, function(x) {dat.stat$dstat[grep(x, dat.taxa$Z)]})
+  Z = lapply(inds, function(x) {dat.stat$Z[grep(x, dat.taxa$p3)]})
 )
 names(out$D) <- names(out$Z) <-
   strsplit(inds, '.', fixed = T) %>% sapply(FUN = '[', 1)
 
 pdf('../OUT/dstat_aegilopsAfares.boxPlot.pdf', 8.5, 11)
-matrix(1:2, 2)
+layout(matrix(1:2, 2))
 boxplot(out$D, cex.axis = 0.5, main = 'D-statistic')
 boxplot(out$Z, cex.axis = 0.5, main = 'Z')
 dev.off()
