a38bb8b610415b3b11cc64365c638e530b770498
andrew-hipp <andrew.hipp@gmail.com>
2022-05-20 15:04:09 -0500

    sapply to return vector of p-values, not list

--- D_stats/r_summarizeTests.R
+++ D_stats/r_summarizeTests.R
@@ -3,8 +3,7 @@
 roundTo = 3 # how many digits to round to
 rt <- function(x, figs = roundTo) round(x, figs)
 mean_quantile <- function(x, lo = 0.025, hi = 0.975) {
-  out <- paste(rt(mean(x)), ' [', rt(quantile(x, lo)), ',', rt(quantile(x, hi)),']', sep  = '')
-  out
+  paste(rt(mean(x)), ' [', rt(quantile(x, lo)), ',', rt(quantile(x, hi)),']', sep  = '')
 }
 
 library(magrittr)
@@ -13,7 +12,7 @@ names(dStats) <- dir(patt = 'bb.dstat.sorted')
 names(dStats) <- gsub('bb.dstat.sorted_|_full.csv', '', names(dStats))
 for(i in seq(length(dStats))) {
   dStats[[i]]$p <-
-    lapply(dStats[[i]]$Z, function(x) {
+    sapply(dStats[[i]]$Z, function(x) {
       out <- pnorm(-sapply(x, abs)) * 2
       out <- p.adjust(out, 'holm')
       return(out)
