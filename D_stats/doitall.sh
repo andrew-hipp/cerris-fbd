09810a37325ef5f8a7674fc0e2904472eaf0eecd
andrew-hipp <andrew.hipp@gmail.com>
2022-05-25 15:55:15 -0500

    do all dstats in doitall.sh

--- D_stats/doitall.sh
+++ D_stats/doitall.sh
@@ -6,13 +6,13 @@ ipcluster start -n 32 --cluster-id="baba" --daemonize
 
 gunzip -k ../DATA/cerrisdstat.loci.gz
 
-# python dStats_suberIlex.py
-# python dStats_cerrisCrenata.py
-# python dStats_libaniCrenata.py
-# python dStats_aegilopsCrenata.py
-# python dStats_cerrisAfares.py
-# python dStats_crenata_v_crenata.py
-# python dStats_partitionedCrenata.py
+python dStats_suberIlex.py
+python dStats_cerrisCrenata.py
+python dStats_libaniCrenata.py
+python dStats_aegilopsCrenata.py
+python dStats_cerrisAfares.py
+python dStats_crenata_v_crenata.py
+python dStats_partitionedCrenata.py
 python dStats_cerrisAfares_libani.py
 python dStats_cerrisAfares_trojana.py
 
