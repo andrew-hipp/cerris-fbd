453d29b441b9b9e918f685299f7b7908709af484
andrew-hipp <andrew.hipp@gmail.com>
2022-05-25 13:27:33 -0500

    move functions to their own script b/c they are used across scripts

---
+++ D_stats/r_zz.functions.R
@@ -0,0 +1,28 @@
+# unique r functions for this project
+
+roundTo = 3 # how many digits to round to
+
+styleIt = function(x, dat = dat.meta, style = c('simple', 'full'),
+            fullCols = c('tipName', 'P', 'label.noDots')) {
+  sampleNum <-
+    strsplit(x, '.', fixed = T) %>% sapply(FUN = '[', 1) %>%
+    gsub(pattern = "OAK-MOR-|OAKS-MOR-", replacement = "")
+  if(style[1] == 'simple') out <- sampleNum %>% as.character
+  if(style[1] == 'full') {
+    rows <- match(x, dat$fastQ.file.name)
+    out <- apply(dat[rows, fullCols], 1, paste, collapse = '|') %>% as.character
+  }
+  return(out)
+} # numbers or taxa for labels
+
+rt <- function(x, figs = roundTo) round(x, figs)
+
+mean_quantile <- mq <- function(x, lo = 0.025, hi = 0.975) {
+  paste(rt(mean(x)), ' [', rt(quantile(x, lo)), ',', rt(quantile(x, hi)),']', sep  = '')
+}
+
+z2holm <- function(z) {
+  out <- pnorm(-sapply(z, abs)) * 2
+  out <- p.adjust(out, 'holm')
+  return(out)
+}
