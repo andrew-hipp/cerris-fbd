415e3406ff15b5be4e0f124f49e76e7298251134
Andrew Hipp <andrew.hipp@gmail.com>
2022-11-02 16:41:23 -0500

    Update to test suber-afares introgression

--- D_stats/dStats_suberAfares.py
+++ D_stats/dStats_suberAfares.py
@@ -17,23 +17,22 @@ newick = '../PHY.NEW/RAxML_bestTree.cerris.2022-01-04.m15.rax'
 bb = ipa.baba(data = locifile, newick = newick)
 
 # Q. afares hybrid origin with suber test constraints:
-#  p4 - Aegilops oaks as outgroups - Q. macrolepis, Q. brantii, Q. ithaburensis
-#  p3 - Q. cerris potential introgressor 1
+#  p4 - Q. chenii, Q. variabilis, and Q. acutissima as outgroups
+#  p3 - Q. suber potential introgressor 1
 #  p2 - Q. afares potential introgressor 2
-#  p1 - Q. libani 628
+#  p1 - undefined --- anyone who fits the topology
 
 bb.generate_tests_from_tree(
     constraint_dict={
-        "p4": ["OAK-MOR-659.fq.barcodeStripped","OAK-MOR-662.fq.barcodeStripped", "OAK-MOR-1055", "OAK-MOR-735.fq.barcodeStripped","OAK-MOR-599.fq.barcodeStripped"],
-        "p3": ["OAK-MOR-591.fq.barcodeStripped","OAK-MOR-736.fq.barcodeStripped", "OAK-MOR-1061", "OAK-MOR-1060", "OAK-MOR-729.fq.barcodeStripped", "OAK-MOR-728.fq.barcodeStripped"],
-        "p2": ["OAK-MOR-983"],
-        "p1": ["OAK-MOR-628.fq.barcodeStripped"]
+        "p4": ["OAK-MOR-982", "OAK-MOR-981", "OAK-MOR-578.fq.barcodeStripped"],
+        "p3": ["OAK-MOR-1144", "OAK-MOR-588.fq.barcodeStripped", "OAK-MOR-985"]
+        "p2": ["OAK-MOR-983"]
     })
 
 bb.run(ipyclient)
 
-out_cerrisAfares_libani = bb.results_table.sort_values(by="Z", ascending=False)
-taxa_cerrisAfares_libani = bb.taxon_table.iloc[out_cerrisAfares_libani.index]
+out_suberAfares = bb.results_table.sort_values(by="Z", ascending=False)
+taxa_suberAfares = bb.taxon_table.iloc[out_suberAfares.index]
 
-out_cerrisAfares_libani.to_csv('bb.dstat.sorted_cerrisAfares_libani_full.csv', sep = ',')
-taxa_cerrisAfares_libani.to_csv('bb.dstat.taxa_cerrisAfares_libani_full.csv', sep = ',')
+out_suberAfares.to_csv('bb.dstat.sorted_suberAfares_full.csv', sep = ',')
+taxa_suberAfares.to_csv('bb.dstat.taxa_suberAfares_full.csv', sep = ',')
