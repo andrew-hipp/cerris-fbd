8fd3fdb5a4075b73adaede5244d201547f6341a0
andrew-hipp <andrew.hipp@gmail.com>
2022-11-16 10:00:20 -0600

    removing single-panel d-stats plots

--- D_stats/r_plot_dStats.R
+++ D_stats/r_plot_dStats.R
@@ -7,24 +7,13 @@ names(dStats) <- gsub('bb.dstat.sorted_|_full.csv', '', names(dStats))
 dStats.d <- lapply(dStats, '[[', 'dstat')
 dStats.d <- dStats.d[sapply(dStats.d, mean) %>% order]
 
-pdf('../OUT/dStats.pdf', 11.5, 8)
-boxplot(dStats.d)
-dev.off()
-
 dStats.Z <- lapply(dStats, '[[', 'Z')[names(dStats.d)]
-pdf('../OUT/dStats-Z.pdf', 11.5, 8)
-boxplot(dStats.Z)
-dev.off()
 
 dStats.p <- lapply(dStats.Z, function(x) {
   out <- pnorm(-sapply(x, abs)) * 2
   out <- p.adjust(out, 'holm')
   return(out)
 })
-pdf('../OUT/dStats-p.pdf', 11.5, 8)
-boxplot(dStats.p, ylim = c(0, 0.3))
-abline(h = 0.01, lty = 'dashed')
-dev.off()
 
 pdf('../OUT/dStats-3panel.pdf', 8, 11.5)
 layout(matrix(1:3, 3))
